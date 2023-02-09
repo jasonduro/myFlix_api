2e1db07a008572a4b6bd01cd1b702412f7dfceeb
Jason <product@jasonlamanque.com>
2023-02-10 00:29:55 +0100

    updates

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express'),
   fs = require('fs'), // import built in node modules fs and path 
   path = require('path');
 
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myflix', { useNewUrlParser: true, useUnifiedTopology: true });
+
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,44 +18,7 @@ const app = express();
 // a ‘log.txt’ file is created in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-let movies = [
-    {
-        title: 'Harry Potter and the Sorcerer\'s Stone',
-        author: 'J.K. Rowling'
-    },
-    {
-        title: 'Lord of the Rings',
-        author: 'J.R.R. Tolkien'
-    },
-    {
-        title: 'Twilight',
-        author: 'Stephanie Meyer'
-    },
-    {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
-    },
-    {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
-    },
-    {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
-    },
-    {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
-    },
-    {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
-    },
-    {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
-    }
-  ];
+
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -68,13 +39,128 @@ app.get('/', (req, res) => {
   res.send('Welcome to my super awesome myFlix movie API app-thing-a-bob!');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
-});
+  //READ Function #1 - Return a list of ALL movies to the user
+  app.get('/movies', (req, res) => {
+    Movies.find()
+        .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+        console.error(error);
+        res.status(500).send('Error: ' + err);
+    }); 
+  }); 
 
-app.get('/movies', (req, res) => {
-    res.send(movies)
-})
+    //READ Function #2 - Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title
+    app.get('/movies/:Title', (req, res) => {
+      Movies.findOne({ Title: req.params.Title })
+      .then((movie) => {
+          res.status(201).json(movie);
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      });
+  }); 
+  
+  //end point that returns only thriller genre movies
+  app.get('/movies/genre', (req, res) => {
+    Movies.find({ 'Genre.Name' : 'Thriller' })
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+  });
+
+  //function to return data about a genre by name
+  app.get('/movies/genre/:Name', (req, res) => {
+    Movies.findOne({ 'Genre.Name': req.params.Name })
+      .then((movies) => {
+        res.status(201).json(movies.Genre);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+  
+  //end point that returns only Jonathan Demme as Director movies
+  app.get('/movies/director', (req, res) => {
+    Movies.find({ 'Director.Name' : 'Jonathan Demme' })
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+  });
+
+  //function to return data about a director by name
+  app.get('/movies/director/:Name', (req, res) => {
+    Movies.findOne({ 'Director.Name': req.params.Name })
+      .then((movies) => {
+        res.status(201).json(movies.Director);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
+    //function to get all users
+    app.get('/users', (req, res) => {
+      Users.find()
+          .then((users) => {
+          res.status(201).json(users);
+      })
+      .catch((err) => {
+          console.error(error);
+          res.status(500).send('Error: ' + err);
+      });
+  });
+
+  //CREATE Function #5 - Allow new users to register
+  app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      }); 
+  }); 
+
+    // Get a user by username - GET Request for specific user based on username
+    app.get('/users/:Username', (req, res) => {
+      Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+          res.json(user);
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      });
+  });
 
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
