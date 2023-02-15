60b51bf221198fcfa0c83cd3304edfe101412936
Jason <product@jasonlamanque.com>
2023-02-15 08:20:42 +0100

    added cors
    added cors allowed origins

--- index.js
+++ index.js
@@ -31,8 +31,23 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
   });
 
+
+//setup Cross-Origin-Resource-Sharing  
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = "The CORS policy for this application does not allow access from origin " + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 //Import auth.js file code
 const auth = require('./auth')(app);
 const passport = require('passport');
