806ed60d50852605d4a4f3b2662cc9135208b14a
Jason <product@jasonlamanque.com>
2023-06-22 08:56:59 +0200

    udpated cors policy

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ app.use((err, req, res, next) => {
 
 //setup Cross-Origin-Resource-Sharing  
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix-app-reloaded.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix-app-reloaded.netlify.app', 'http://localhost:4200' ];
 
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
       return callback(new Error(message), false);
     }
     return callback(null, true);
