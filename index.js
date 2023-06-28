421788b7c39318055479f6bc2a450ee31387a53d
Jason <product@jasonlamanque.com>
2023-06-28 15:28:00 +0200

    updated endpoint to json

--- index.js
+++ index.js
@@ -278,14 +278,14 @@ app.get('/', (req, res) => {
         Users.findOneAndRemove({ Username: req.params.Username })
         .then((user) => {
             if (!user) {
-            res.status(400).send(req.params.Username + ' was not found');
+            res.status(400).json({ message: req.params.Username + ' was not found'});
             } else {
-            res.status(200).send(req.params.Username + ' was deleted.');
+            res.status(200).json({ message: req.params.Username + ' was deleted.'});
             }
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).send('Error: ' + err);
+            res.status(500).json({ message: 'Error: ' + err });
         });
     });
 
